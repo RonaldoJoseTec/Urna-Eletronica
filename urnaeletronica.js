3762be5ebde9624e469270b51ca3663d24b68f3f
RonaldoJoseTec <ronaldo.japs@gmail.com>
2023-10-18 11:54:08 -0300

    feat: seguindo o desafio 5

--- urnaeletronica.js
+++ urnaeletronica.js
@@ -9,6 +9,8 @@ function urnaeletronica() {
     let nome2 = 0 ;
     let nome3 = 0 ;
     let contador = 0;
+    let senha ;
+    senha = prompt('Digite a senha de segurança')
     nome1 = prompt('Digite o nome do candidato 1');
     nome2 = prompt('Digite o nome do candidato 2');
     nome3 = prompt('Digite o nome do candidato 3');
@@ -17,31 +19,45 @@ function urnaeletronica() {
     let candidato3 = 0;
     let branco = 0;
     let nulo = 0;
+    let decisao = 'N' ;
 
     do {
-        opcao = parseInt(prompt('digite a opção'));
+        opcao = parseInt(prompt('digite a opção \n' 
+        + '|1|' +nome1+ '\n'
+        + '|2|' +nome2+ '\n'
+        + '|3|' +nome3+ '\n'
+        + '|5| branco \n'
+        + '|8| nulo \n'
+        + ' digite a senha para encerrar '));
         console.log('linha de instrução');
         contador++;
         if (opcao == 1) {
             candidato1++;
             console.log('você votou no candidato1');
+            alert('voto computado com sucesso !');
         } else if (opcao == 2) {
             candidato2++;
             console.log('você votou no candidato2');
+            alert('voto computado com sucesso !');
         } else if (opcao == 3) {
             candidato3++;
             console.log('você votou no candidato3');
+            alert('voto computado com sucesso !');
         } else if (opcao == 5) {
             branco++;
             console.log('você votou em branco');
+            alert('voto computado com sucesso !');
         } else if (opcao == 8) {
             nulo++;
             console.log('você votou nulo');
-        } else if(opcao == 0) {
-            prompt('deseja encerrar a votação');
-            
+            alert('voto computado com sucesso !');
+        } else if(opcao == senha) {
+            decisao = prompt('deseja encerrar a votação \n S para sim , N para não'); 
+        } else if(opcao != 1,2,3,5,8) {
+            alert('opção inválida !')
+            function confirm('pffff')
         }
-    } while (opcao != 0);
+    } while (decisao !== 'S');
 
     contador = contador - 1 ;
     console.log('contagem', contador);
@@ -55,15 +71,15 @@ function urnaeletronica() {
     console.log('total dos votos', totaldevotos);
 
     console.log('relação do candidato', nome1, candidato1);
-    console.log('percentual de votos do candidato1', (candidato1 / totaldevotos) * 100 +'%');
+    console.log('percentual de votos do candidato1', ((candidato1 / totaldevotos * 100).toFixed(2)) +'%');
     console.log('relação do candidato', nome2, candidato2);
-    console.log('percentual de votos do candidato2', (candidato2 / totaldevotos) * 100 +'%');
+    console.log('percentual de votos do candidato2', ((candidato2 / totaldevotos * 100).toFixed(2)) +'%');
     console.log('relação do candidato', nome3, candidato3);
-    console.log('percentual de votos do candidato3', (candidato3 / totaldevotos) * 100 +'%');
+    console.log('percentual de votos do candidato3', ((candidato3 / totaldevotos * 100).toFixed(2)) +'%');
     console.log('relação de votos em branco', branco);
-    console.log('percentual de votos em branco ', (branco / totaldevotos) * 100 +'%');
+    console.log('percentual de votos em branco ', ((branco / totaldevotos).toFixed(2)) * 100 +'%');
     console.log('relação de votos nulos', nulo);
-    console.log('percentual de votos nulos', (nulo / totaldevotos) * 100 +'%');
+    console.log('percentual de votos nulos', ((nulo / totaldevotos).toFixed(2)) * 100 +'%');
     
 
     if (candidato1 > candidato2 && candidato1 > candidato3) {
