8c80e2378f75bef22c5ce5e059e2ab76b168da27
RonaldoJoseTec <ronaldo.japs@gmail.com>
2023-10-30 10:42:42 -0300

    feat: desafio 9 concluido (talvez)

--- urnaeletronica.js
+++ urnaeletronica.js
@@ -23,11 +23,11 @@ function urnaeletronica() {
     //const AudioConfirmacao = Audio();
     const dataInicio = new Date();
     const matrizCandidato = [
-        ['Nº 17','Candidato: Alfa'],
-        ['Nº 23', 'Candidato: Beta'],
-        ['Nº 35', 'Candidato: Charlie'],
-        ['Nº 41', 'Candidato: Dom'],
-        ['Nº 52', 'Candidato: Esquiel']
+        ['Nº 17', ' Candidato: Alfa'],
+        ['Nº 23', ' Candidato: Beta'],
+        ['Nº 35', ' Candidato: Charlie'],
+        ['Nº 41', ' Candidato: Dom'],
+        ['Nº 52', ' Candidato: Esquiel']
     ];
     let nome1 = matrizCandidato[0] ;
     let nome2 = matrizCandidato[1] ;
@@ -37,28 +37,28 @@ function urnaeletronica() {
       
     console.log('Iniciando o programa às: ' + dataInicio.toLocaleString());
 
-   // console.clear();
+    //console.clear();
     console.log(' *** Iniciando as configurações da URNA *** ' );
 
     senha = parseInt(prompt('Digite a senha de segurança'));
 
     
     do {
-        //for (i=0; i<=4; i++) {
+        for (i=0; i<=4; i++) {
             //for (j=0; j<=4; j++) {}
-                //console.log('Candidaturas: ' + matrizCandidato[i]);
-           // }
+                console.log('Candidaturas: ' + matrizCandidato[i]);
+            }
         
         //nome1 = prompt('Digite o nome do candidato 1');
         //nome2 = prompt('Digite o nome do candidato 2');
         //nome3 = prompt('Digite o nome do candidato 3');
 
-        console.log('>>> Nomes dos Candidatos <<<');
+        /*console.log('>>> Nomes dos Candidatos <<<');
         console.log('|1| Candidato 1: ' + nome1);
         console.log('|2| Candidato 2: ' + nome2);
         console.log('|3| Candidato 3: ' + nome3);
         console.log('|4| Candidato 4: ' + nome4);
-        console.log('|5| Candidato 5: ' + nome5);
+        console.log('|5| Candidato 5: ' + nome5);*/
 
     } while (!confirm('Se os nomes dos candidatos estiverem corretos, clique em "Ok" para iniciar a votação ou "Cancelar" para corrigir as candidaturas'));
 
@@ -166,17 +166,17 @@ function urnaeletronica() {
     // se houver votação
     if (contadorDeVotos > 0) {
 
-        console.log('relação do candidato', nome1, candidato1);
+        console.log('relação do candidato', nome1[1], candidato1);
         console.log('percentual de votos do candidato1', ((candidato1 / contadorDeVotos * 100).toFixed(2)) + '%');
-        console.log('relação do candidato', nome2, candidato2);
+        console.log('relação do candidato', nome2[1], candidato2);
         console.log('percentual de votos do candidato2', ((candidato2 / contadorDeVotos * 100).toFixed(2)) + '%');
-        console.log('relação do candidato', nome3, candidato3);
+        console.log('relação do candidato', nome3[1], candidato3);
         console.log('percentual de votos do candidato3', ((candidato3 / contadorDeVotos * 100).toFixed(2)) + '%');
         console.log('relação de votos em branco', branco);
         console.log('percentual de votos do candidato4', ((candidato4 / contadorDeVotos * 100).toFixed(2)) + '%');
-        console.log('relação do candidato', nome4, candidato4);
+        console.log('relação do candidato', nome4[1], candidato4);
         console.log('percentual de votos do candidato5', ((candidato5 / contadorDeVotos * 100).toFixed(2)) + '%');
-        console.log('relação do candidato', nome5, candidato5);
+        console.log('relação do candidato', nome5[1], candidato5);
         console.log('percentual de votos em branco ', ((branco / contadorDeVotos * 100).toFixed(2)) + '%');
         console.log('relação de votos nulos', nulo);
         console.log('percentual de votos nulos', ((nulo / contadorDeVotos * 100).toFixed(2)) + '%');
